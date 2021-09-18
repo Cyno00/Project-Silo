6446e0ffb1f8a8504843f711ef21e696b288a14e
yangster67 <kyang6671@gmail.com>
2021-09-18 13:12:24 -0400

    Added a functional dropdown list

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-
+function filterFunction() {
+  var count = 0; 
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = document.getElementsByClassName("listElement");
+  var empty = document.getElementById("blankMessage");
+  
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+	  count++;
+    } else {
+	  if (a[i].id != "blankMessage") {
+		a[i].style.display = "none";
+	  }
+    }
+	
+  }
+}
