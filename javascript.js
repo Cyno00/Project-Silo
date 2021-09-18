62523bef27bc8f30dc82c16933acc421c96f7222
yangster67 <kyang6671@gmail.com>
2021-09-18 14:18:27 -0400

    Added descriptions in list results

--- javascript.js
+++ javascript.js
@@ -25,13 +25,16 @@ function listClick(elmt) {
 	var result = document.getElementById("result");
 	var id = elmt.id;
 	if (id === "apple") {
-		result.innerHTML = "apple";
+		result.innerHTML = 'Apple devices can be recyceld via Apple Trade In, where store credit <br> can be given: <a href="https://www.apple.com/shop/trade-in"> Link </a> <br> <br> There are also many other providers that will accept your device.';
 	}
 	else if (id === "samsung") {
-		result.innerHTML = "samsung";
+		result.innerHTML = 'Samsung has many recycle programs at no cost, which can be found <a href="https://www.samsung.com/us/aboutsamsung/sustainability/environment/responsible-recycling/programs/"> here. </a>';
 	}
-	else if (id === "blankMessage") {
-		result.innerHTML = "other";
+	else if (id === "google") {
+		result.innerHTML = 'You can recycle most Google devices with their recycling program <a href= "https://store.google.com/us/magazine/recycling?hl=en-US">here.</a> <br> You can also use the trade-in program to recieve store credit when buying a new Pixel.';
+	}
+	else if (id === "emptyMessage") {
+		result.innerHTML = 'We could not find a recycling program that is dedicated to your device, but there are many organizations that help recycle most electronics. <a href = "https://call2recycle.org"> Call2Recycle </a> and many <br> other websites will allow you to find nearby recycling centers after inputting your ZIP code.';
 	}
 	else {
 		result.innerHTML = "this is a bug";
